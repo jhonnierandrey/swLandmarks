293b2f1e8b3f7f76540fcce06d1b697d899f7208
jhonnierandrey <jhonnierandrey@outlook.com>
2023-08-09 18:22:13 -0300

    0809 - SwiftUI Essentials completed

---
+++ Landmarks/Views/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Jhonnier Zapata on 8/9/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .environmentObject(ModelData())
+    }
+}
---
+++ Landmarks/Views/Helpers/CircleImage.swift
@@ -0,0 +1,26 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by Jhonnier Zapata on 8/9/23.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var image: Image
+    var body: some View {
+        image
+            .clipShape(Circle())
+            .overlay {
+                Circle().stroke(.white, lineWidth: 4)
+            }
+            .shadow(radius : 7)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage(image: Image("turtlerock"))
+    }
+}
