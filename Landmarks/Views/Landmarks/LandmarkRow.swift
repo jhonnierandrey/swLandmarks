9c510655311c93b72155f51e5d5873224126c313
jhonnierandrey <jhonnierandrey@outlook.com>
2023-08-13 15:49:41 -0300

    0813 - Framework Integration : Creating a macOS App completed

--- Landmarks/Views/Landmarks/LandmarkRow.swift
+++ Landmarks/Views/Landmarks/LandmarkRow.swift
@@ -15,7 +15,16 @@ struct LandmarkRow: View {
             landmark.image
                 .resizable()
                 .frame(width: 50, height: 50)
-            Text(landmark.name)
+                .cornerRadius(5)
+            VStack(alignment: .leading){
+                Text(landmark.name)
+                    .bold()
+                #if !os(watchOS)
+                Text(landmark.park)
+                    .font(.caption)
+                    .foregroundColor(.secondary)
+                #endif
+            }
             Spacer()
             
             if landmark.isFavorite {
@@ -23,7 +32,7 @@ struct LandmarkRow: View {
                     .foregroundColor(.yellow)
             }
         }
-        .padding()
+        .padding(.vertical, 4)
     }
 }
 
---
+++ MacLandmarks/LandmarkCommands.swift
@@ -0,0 +1,34 @@
+//
+//  LandmarkCommands.swift
+//  Landmarks
+//
+//  Created by Jhonnier Zapata on 8/13/23.
+//
+
+import SwiftUI
+
+struct LandmarkCommands : Commands {
+    @FocusedBinding(\.selectedLandmark) var selectedLandmark
+    
+    var body: some Commands {
+        SidebarCommands()
+        CommandMenu("Landmark"){
+            Button("\(selectedLandmark?.isFavorite == true ? "Remove" : "Mark") as Favorite"){
+                selectedLandmark?.isFavorite.toggle()
+            }
+            .keyboardShortcut("f", modifiers: [.shift, .option])
+            .disabled(selectedLandmark == nil)
+        }
+    }
+}
+
+private struct SelectedLandmarkKey: FocusedValueKey {
+    typealias Value = Binding<Landmark>
+}
+
+extension FocusedValues {
+    var selectedLandmark: Binding<Landmark>? {
+        get { self[SelectedLandmarkKey.self] }
+        set { self[SelectedLandmarkKey.self] = newValue}
+    }
+}
